2421a626ab3418b3f855deeedd5c5bea8dc7fb39
Anthony Patterson <a.patterson@ucc.ie>
2012-05-22 14:36:04 +0100

    pass through underlying map functionality

--- src/coralfish/views/welcome.clj
+++ src/coralfish/views/welcome.clj
@@ -2,32 +2,43 @@
   (:require [coralfish.views.common :as common]
             [noir.content.getting-started])
   (:use [noir.core :only [defpage]]
+        [noir.response :only [redirect]]
         [hiccup.core :only [html]]
         [net.cgrand.enlive-html])
   (:import java.net.URL))
 
-defn gis
-  [rest]
-  (transform (html-resource (URL. (str "http://coralfish.ucc.ie/" rest)))
-             [:head]
-             (append {:tag :script 
-                      :attrs {:type "text/javascript"}
-                      :content (str 
-"function onShowInfoLayerClick ( layerID ) {"
-    "windowURL = 'http://www.climateireland.ie:8080/svg/compare/2021/2035/J2D/T_2M';"
-    "window_handle = window.open( windowURL, 'popupDOI',"
-                                 "'toolbar=no,status=yes,resizable=yes,scrollbars=yes,width=800,height=700,top=160,left=160' );"
-    "window_handle.focus();"
-    "return false;"
-"}")})))
+(defn gis
+  [keys]
+  (let [{:keys [rest layer]} keys
+        keys2 (dissoc keys :layer)
+        layers (apply str (interpose \& (map #(str "layer[]=" %) layer)))
+        param_str (apply str (interpose \& (map #(str (name (key %)) "=" (val %)) keys2)))
+        url (URL. (str "http://coralfish.ucc.ie/index.php?" param_str "&" layers))]
+	(println url)
+    (transform (html-resource url) 
+               [:head]
+               (append {:tag :script 
+                        :attrs {:type "text/javascript"}
+                        :content (str 
+                                 "function onShowInfoLayerClick ( layerID ) {"
+                                    "windowURL = 'http://www.climateireland.ie:8080/svg/compare/2021/2035/J2D/T_2M';"
+                                    "window_handle = window.open( windowURL, 'popupDOI',"
+                                    "'toolbar=no,status=yes,resizable=yes,scrollbars=yes,width=800,height=700,top=160,left=160' );"
+                                    "window_handle.focus();"
+                                    "return false;"
+                                    "}")}))))
 
 (defpage "/noir" []
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body (emit* (gis))})
 
-(defpage ["/noir/:rest" :rest #".*$"]
-  {:keys [rest]}
+(defpage [:get ["/noir/:rest" :rest #"(index|mode)\.php.*"]]
+  keys
   {:status 200
    :headers {"Content-Type" "text/html"}
-   :body (emit* (gis rest))})
+   :body (emit* (gis keys))})
+
+(defpage [:get ["/noir/:first/:rest" :rest #".*$"]]
+  {:keys [first rest]}
+  (redirect (str "/" first "/" rest)))
