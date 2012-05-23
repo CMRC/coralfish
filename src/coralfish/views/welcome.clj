db8e0c432b1ce96cee319760a3ed2df0ca7d927d
Anthony Patterson <a.patterson@ucc.ie>
2012-05-23 15:32:18 +0100

    fixed some problems with l layer selection

--- src/coralfish/views/welcome.clj
+++ src/coralfish/views/welcome.clj
@@ -5,7 +5,8 @@
         [noir.response :only [redirect]]
         [hiccup.core :only [html]]
         [net.cgrand.enlive-html])
-  (:import java.net.URL))
+  (:import java.net.URL 
+           java.net.URLEncoder))
 
 (defn gis
   [keys]
@@ -13,28 +14,22 @@
         keys2 (dissoc keys :layer)
         layers (apply str (interpose \& (map #(str "layer[]=" %) layer)))
         param_str (apply str (interpose \& (map #(str (name (key %)) "=" (val %)) keys2)))
-        url (URL. (str "http://coralfish.ucc.ie/index.php?" param_str "&" layers))]
-	(println url)
+        url (URL. (str "http://coralfish.ucc.ie/" rest "?" param_str "&" layers))]
     (transform (html-resource url) 
-               [:head]
-               (append {:tag :script 
-                        :attrs {:type "text/javascript"}
-                        :content (str 
-                                 "function onShowInfoLayerClick ( layerID ) {"
-                                    "windowURL = 'http://www.climateireland.ie:8080/svg/compare/2021/2035/J2D/T_2M';"
-                                    "window_handle = window.open( windowURL, 'popupDOI',"
-                                    "'toolbar=no,status=yes,resizable=yes,scrollbars=yes,width=800,height=700,top=160,left=160' );"
-                                    "window_handle.focus();"
-                                    "return false;"
-                                    "}")}))))
+               [:#mapserverForm :> :input]
+               (fn [a-node] (let [id (keyword (:id (:attrs a-node)))
+                                  par (when id (id keys))]
+                                 (println id " = " par)
+                                     (assoc-in a-node [:attrs :value] par))))))
 
 (defpage "/noir" []
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body (emit* (gis))})
 
-(defpage [:get ["/noir/:rest" :rest #"(index|mode)\.php.*"]]
+(defpage [:get ["/noir/:rest" :rest #".*\.php"]]
   keys
+  (println keys)
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body (emit* (gis keys))})
@@ -42,3 +37,7 @@
 (defpage [:get ["/noir/:first/:rest" :rest #".*$"]]
   {:keys [first rest]}
   (redirect (str "/" first "/" rest)))
+
+(defpage [:get ["/noir/:first/:second/:rest" :rest #".*$"]]
+  {:keys [first second rest]}
+  (redirect (str "/" first "/" second "/" rest)))
